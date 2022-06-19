c58702ed5c07207147d29a84775a8eadf758f85b
biancanecy <106227810+biancanecy@users.noreply.github.com>
2022-06-19 10:25:54 -0300

    functions

---
+++ functions/function.js
@@ -0,0 +1,19 @@
+//um aplicativo sobre frases motivacionais:
+
+
+
+//declaração de função:
+
+function createPhrases(){
+
+    console.log('Estudar é bom')
+    console.log('tenha mais paciência')
+    console.log('tudo vai dar certo')
+
+}
+
+//como executar, rodar ou chamar a função:
+
+createPhrases()
+console.log('fim do programa')
+
