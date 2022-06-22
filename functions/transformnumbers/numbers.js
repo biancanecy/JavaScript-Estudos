e160c66da8aa867a430a425204b530d343912587
biancanecy <106227810+biancanecy@users.noreply.github.com>
2022-06-21 21:30:17 -0300

    upper and lowercase

--- functions/transformnumbers/numbers.js
+++ functions/transformnumbers/numbers.js
@@ -3,3 +3,13 @@
 
 //let number = 345.33854584985
 //console.log(number.toFixed(2).replace(',','.')) //replace para trocar ponto por virgula
+
+
+
+//transformando letras minusculas em maiusculas e vice versa:
+
+let word = 'adoro programar'
+console.log(word.toUpperCase()) //tranformar em letra maiuscula o conteudo de word
+
+let world = 'adoro programar'
+console.log(word.toLowerCase()) //tranformar em letra maiuscula o conteudo de world
