ce6a1e319f29996f10372762ba696862eb78f8e3
biancanecy <106227810+biancanecy@users.noreply.github.com>
2022-06-21 21:16:23 -0300

    tranformando strings

---
+++ functions/transformnumbers/numbers.js
@@ -0,0 +1,5 @@
+
+//transformando um numero quebrado com duas casas decimais e trocando por ponto e virgula;
+
+//let number = 345.33854584985
+//console.log(number.toFixed(2).replace(',','.')) //replace para trocar ponto por virgula
