6d0eb174606de72b87289dcbc677f402b7a78271
biancanecy <106227810+biancanecy@users.noreply.github.com>
2022-06-23 12:23:15 -0300

    operador condicional ternário

--- operadores/opercond.js
+++ operadores/opercond.js
@@ -1,10 +1,20 @@
 
 //OPERADOR CONDICIONAL TERNÁRIO;
 
-let pao = true
+/* let pao = true
 let queijo = true
 
 const cafebom = pao && queijo ? 'café bom':'café ruim'
 
-console.log(cafebom)
+console.log(cafebom) */
+
+//CASO APENAS UMA DAS OPÇÕES DISPONIVEIS SEJA SUFICIENTE:
+
+/* let pao = false
+let queijo = true
+
+const cafebom = pao || queijo ? 'café bom' : 'café ruim'
+
+console.log(cafebom) */
+
 
