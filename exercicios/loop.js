a1e86c088cb037478f4bfdb6c81a5a07dcef181b
biancanecy <106227810+biancanecy@users.noreply.github.com>
2022-06-25 11:40:18 -0300

    loops

---
+++ exercicios/loop.js
@@ -0,0 +1,26 @@
+
+/*
+    for (let i = 100; i > 0; i--){
+        if (i===50){
+            break;
+        }
+
+
+
+
+        console.log(i)
+
+    } */
+
+    //COMANDO WHILE : ENQUANTO;
+    //OUTRA ESTRUTURA DE REPETIÇÃO USADA QUANDO NÃO SE SABE QUANDO PARAR
+
+    let i = 4598765;
+
+    while(i>10){ //irá se repetir enquando i for maior que dez, ou seja pra sempre
+        console.log(i)
+
+
+        i/=35 //irá parar enquanto o resultado do i divido por 35 for menor que 10
+
+    }
