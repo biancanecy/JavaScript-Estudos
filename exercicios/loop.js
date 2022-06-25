f58c94fbc385bbacba608e70d2d5a5112d2afad4
biancanecy <106227810+biancanecy@users.noreply.github.com>
2022-06-25 11:54:02 -0300

    loops, for of

--- exercicios/loop.js
+++ exercicios/loop.js
@@ -15,12 +15,28 @@
     //COMANDO WHILE : ENQUANTO;
     //OUTRA ESTRUTURA DE REPETIÇÃO USADA QUANDO NÃO SE SABE QUANDO PARAR
 
-    let i = 4598765;
+  /*  let i = 4598765;
 
     while(i>10){ //irá se repetir enquando i for maior que dez, ou seja pra sempre
         console.log(i)
 
 
         i/=35 //irá parar enquanto o resultado do i divido por 35 for menor que 10
+
+    }    */
+
+
+    //FOR OF: CAPTURAR UM CARACTERE DE UMA STRING, OU UM ELEMENTO DE UM ARRAY
+
+    let name = 'bia' //string
+    let names = ['joao', 'pedro','lucas'] //array
+
+    for(let char of name){
+        console.log(char)
+
+    }
+
+    for (let name of names){
+        console.log(name)
 
     }
